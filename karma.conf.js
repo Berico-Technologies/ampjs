76a895a0bb5c907b46f7ae76340615a54d3c7d71
Drew Tayman <atayman@bericotechnologies.com>
2013-05-23 13:26:50 -0400

    karma test harness with a dummy test using assert.js

---
+++ karma.conf.js
@@ -0,0 +1,64 @@
+
+// list of files / patterns to load in the browser
+files = [
+  MOCHA,
+  MOCHA_ADAPTER,
+  REQUIRE,
+  REQUIRE_ADAPTER,
+  'test/vendor/assert.js',
+  'test/test-main.js',
+  {pattern: 'public/javascripts/**/*.js', included: false},
+  {pattern: 'test/**/*Spec.js', included: false}
+];
+
+
+// list of files to exclude
+exclude = [
+  'public/javascripts/src/main.js'
+];
+
+
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['progress'];
+
+
+// web server port
+port = 9876;
+
+
+// cli runner port
+runnerPort = 9100;
+
+
+// enable / disable colors in the output (reporters and logs)
+colors = true;
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
+
+
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+browsers = ['PhantomJS'];
+
+
+// If browser does not capture in given timeout [ms], kill it
+captureTimeout = 60000;
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
---
+++ test/testOneSpec.js
@@ -0,0 +1,14 @@
+define(['jquery', 'underscore'], function($, _) {
+
+
+
+    describe('just checking', function() {
+
+        it('works for underscore', function() {
+            // just checking that _ works
+            assert.equal(_.size([1,2,3]), 3);
+        });
+
+    });
+
+});
